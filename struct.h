4110456e9ab019dff5cabb99e65b50588d6a5cb4
gpffh20 <eunbin584836@gmail.com>
2024-01-02 22:49:07 +0900

    Feat: 헤더파일 minishell 하나만 include하기 위한 헤더파일 생성

--- struct.h
+++ struct.h
@@ -6,6 +6,50 @@
 # define TRUE 1
 # define FALSE 0
 
+// share
+typedef enum e_type
+{
+	PIPE = 1,
+	OUT_REDIR,
+	IN_REDIR,
+	APPEND_REDIR,
+	HEREDOC
+}	t_type;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct s_redir
+{
+	t_type			type;
+	char			*filename;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_cmd
+{
+	int				cmd_cnt;
+	char			**cmd_args;
+	t_redir			*redir;
+	struct s_cmd	*next;
+}	t_cmd;
+
+typedef struct s_shell
+{
+	int		pipe_cnt;
+	int		heredoc_cnt;
+	t_env	*env;
+	t_cmd	*cmd;
+}	t_shell;
+
+// parse
+
+
+// execute
 typedef struct s_exec
 {
 	int		pipe[2];
