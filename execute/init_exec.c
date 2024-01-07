1e1d7d4977098f82682f693b8324772812e6b609
gpffh20 <eunbin584836@gmail.com>
2024-01-07 23:27:57 +0900

    refactor init_exec에 malloc & memset 추가

--- execute/init_exec.c
+++ execute/init_exec.c
@@ -53,6 +53,8 @@ void	make_new_env(t_exec *exec_info)
 
 void	init_exec(t_shell *shell_info, t_exec *exec_info)
 {
+	exec_info = malloc(sizeof(t_exec));
+	ft_memset(exec_info, 0, sizeof(t_exec));
 	exec_info->path = get_path(exec_info);
 	exec_info->env = shell_info->env;
 	make_new_env(exec_info);
