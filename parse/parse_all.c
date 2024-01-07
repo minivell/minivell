689458df7093288376938254b3bbe9ecb5e3315f
gykoh42 <karmakoh42@naver.com>
2024-01-07 20:00:34 +0900

    feat: parse_redir 함수 추가 및 테스트 코드 첨부
    아직 공백도 WORD 타입으로 처리되고 있어 추후 이 부분 수정할 예정

--- parse/parse_all.c
+++ parse/parse_all.c
@@ -1,11 +1,23 @@
 #include "../minishell.h"
 
-int	parse_all(/*t_shell *shell_info, char *str*/)
+int	parse_all(/*t_shell *shell_info, */char *str)
 {
+	t_token *token;
+	token = NULL;
 	// 1. parsing 전 에러 handling (>> 갯수, 파이프 닫혔는지 등 확인) -> 나중에
 
 	// 2. 파이프 단위로 token 처리
+	parse_pipe(&token, str);
 	// 3. 리다이렉션 처리
+	parse_redir(&token);
+
+	t_token *current_token;
+	current_token = token;
+    while (current_token)
+    {
+        printf("Type: %d, Value: [%s]\n", current_token->type, current_token->value);
+        current_token = current_token->next;
+    }
 	// 4. white space 처리 -> token 완성
 	// 5. token 순회하면서 invalid token 삭제
 
