a9da06587c3c519e921445312cfc2fac8ed889da
gykoh42 <karmakoh42@naver.com>
2024-01-07 19:55:47 +0900

    feat: redir 관련 parsing 함수 추가

--- minishell.h
+++ minishell.h
@@ -20,11 +20,20 @@
 /*parse*/
 void	init_env(t_env **env, char *envp[]);
 void 	parse_pipe(t_token **token, char *str);
-int		set_quote(t_quote *quote, char c);
+
+int		check_quote(t_quote *quote, char c);
+
+void	parse_redir(t_token **token);
+void	parse_and_extract_redir(t_token **token, char *str);
+void	replace_token(t_token **token_list, t_token *old_token, t_token *new_tokens);
+void	identify_redir(t_token **token, char **str, char *start_str);
+void create_redir_token(t_token **token, char **str);
+
 void	add_token_if_not_empty(char **start, char **current, t_token **token, t_type type);
-t_token	*token_new_node(t_type type, char *value);
-void	token_add_back(t_token **node, t_token *new);
+t_token	*new_token(t_type type, char *value);
+void	add_back_token(t_token **node, t_token *new);
 void	free_token(t_token *token);
+
 int		parse_all(/*t_shell *shell_info, */char *str);
 
 #endif
