859876d12e5a1aa40d197de4770e8229619a42ae
gykoh42 <karmakoh42@naver.com>
2024-01-07 21:51:35 +0900

    feat: identify_redir 함수 삭제 및 parse_space 함수 추가

--- minishell.h
+++ minishell.h
@@ -27,13 +27,14 @@ int		check_quote(t_quote *quote, char c);
 void	parse_redir(t_token **token);
 void	parse_and_extract_redir(t_token **token, char *str);
 void	replace_token(t_token **token_list, t_token *old_token, t_token *new_tokens);
-void	identify_redir(t_token **token, char **str, char *start_str);
-void create_redir_token(t_token **token, char **str);
+void 	create_redir_token(t_token **token, char **str);
 
 void	add_token_if_not_empty(char **start, char **current, t_token **token, t_type type);
 t_token	*new_token(t_type type, char *value);
 void	add_back_token(t_token **node, t_token *new);
 void	free_token(t_token *token);
+
+void 	parse_space(t_token **token);
 
 int		parse_all(/*t_shell *shell_info, */char *str);
 
