fb2bbde9ee0fda14779bec5d78630cb6c8dd26f5
gpffh20 <eunbin584836@gmail.com>
2024-01-08 13:49:14 +0900

    fix: 옵션 처리 완료

--- builtin/echo.c
+++ builtin/echo.c
@@ -1,8 +1,5 @@
 #include "../minishell.h"
 
-// -nnnnnnn -> 정상작동 처리
-// -n -n -n -n -n -> 정상작동 처리
-// -n-n-n-n-n-n -> 그냥 하나의 문자열로 처리
 void	echo(char **args)
 {
 	int	idx;
@@ -10,7 +7,7 @@ void	echo(char **args)
 
 	idx = 1;
 	newline = TRUE;
-	if (args[idx] && ft_strncmp(args[idx], "-n", 2) == TRUE)
+	while (args[idx] && ft_strncmp(args[idx], "-n ", 3) == TRUE)
 	{
 		newline = FALSE;
 		idx++;
