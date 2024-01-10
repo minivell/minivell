8492866a60f3b9108d601c0f1e0df296e9818157
gpffh20 <eunbin584836@gmail.com>
2024-01-10 18:26:02 +0900

    feat: parsing된 cmd 넘겨받는 로직 추가

--- exec_main.c
+++ exec_main.c
@@ -1,31 +1,37 @@
 #include "minishell.h"
 int	g_exit_code;
 
+//void	_check(void)
+//{
+//	system("leaks minishell_exec");
+//}
+
 int main(int ac, char *av[], char *envp[])
 {
+//	atexit(_check);
+	t_shell shell_info;
 	char	*str;
-	t_shell	shell_info;
+	(void) ac;
+	(void) av;
 
 	init_env(&shell_info.env, envp);
 	while (TRUE)
 	{
 		str = readline("minivell$ ");
-		if (!str)
+		if (str == NULL)
 			break ;
-		execute(&shell_info);
+		if (!*str)
+		{
+			free(str);
+			continue ;
+		}
+		if (parse_all(&shell_info, str) == SUCCESS)
+		{
+//			shell_info.pipe_cnt = 1; // for test multi process
+			execute(&shell_info);
+		}
 		add_history(str);
 		free(str);
 	}
-	(void) ac;
-	(void) av;
-	(void) envp;
 	return (g_exit_code);
-	//	text code
-//	t_env *current_env;
-//
-//	current_env = shell_info.env;
-//	while (current_env != NULL) {
-//		printf("Key: %s, Value: %s\n", current_env->key, current_env->value);
-//		current_env = current_env->next;
-//	}
 }
