795c9f0f14f80a1e63b1705e2a2e76cf1d5bbb95
gykoh42 <karmakoh42@naver.com>
2024-01-10 17:31:35 +0900

    chore: 필요없는 주석 삭제

--- parse/parse_all.c
+++ parse/parse_all.c
@@ -7,13 +7,12 @@ int	parse_all(t_shell *shell_info, char *str)
 	token = NULL;
 	// 1. parsing 전 에러 handling (>> 갯수, 파이프 닫혔는지 등 확인) -> 나중에
 
-	// 2. 파이프 단위로 token 처리
 	parse_pipe(&token, str);
-	// 3. 리다이렉션 처리
 	parse_redir(&token);
-	// 4. white space 처리 -> token 완성
 	parse_space(&token);
+	// parse_quote(&token);
 
+	// token test code
 	// t_token *current_token;
 	// current_token = token;
     // while (current_token)
@@ -23,23 +22,11 @@ int	parse_all(t_shell *shell_info, char *str)
     // }
 	
 	// 5. token 순회하면서 invalid token 삭제
-
 	// 6. token 없거나 에러 걸리면 token free하고 fail return -> 나중에
-
 	// 7. envp update -> 은영 실행부에서
-	
 	// 8. cmd init
 	// init_cmd(&shell_info->cmd, token);
 	// 9. cmd argument 합치기
 
-	t_cmd *cmd = tokens_to_cmds(token);
-	if (cmd != NULL)
-    {
-        printf("Command Arguments Count: %d\n", cmd->cmd_cnt);
-        for (int i = 0; i < cmd->cmd_cnt; i++)
-        {
-            printf("Argument %d: %s\n", i, cmd->cmd_args[i]);
-        }
-    }
 	return (SUCCESS);
 }
