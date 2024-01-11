0dfdcb4f3f39e6a08e0aacbf9e294759979f59a9
gykoh42 <karmakoh42@naver.com>
2024-01-11 21:09:42 +0900

    feat: error_before_parse 함수 및 count_token_type 함수 추가

--- parse/parse_all.c
+++ parse/parse_all.c
@@ -4,15 +4,32 @@ int	parse_all(t_shell *shell_info, char *str)
 {
 	t_token *token;
 	token = NULL;
-	// 1. parsing 전 에러 handling (>> 갯수, 파이프 닫혔는지 등 확인) -> 나중에
-
+	shell_info->heredoc_cnt = 0;
+	if (error_before_parse(token, str) == EXIT_FAILURE)
+    	return (EXIT_FAILURE);
 	parse_pipe(&token, str);
 	parse_redir(&token);
 	parse_space(&token);
 	parse_filename(&token);
-	// parse_quote(&token);
+	
+	// shell_info->heredoc_cnt = 0;
+	// shell_info->pipe_cnt = 0;
 
-	// token test code
+	count_token_type(shell_info, token);
+	// printf("heredoc: %d, pipe: %d\n", shell_info->heredoc_cnt, shell_info->pipe_cnt);
+
+	// if (!token || token_error(token) != EXIT_SUCCESS)
+	// {
+	// 	free_token(token);
+	// 	return (EXIT_FAILURE);
+	// }
+
+	// if (!token || error_before_parse(shell_info, token, str))
+	// {
+	// 	printf("%d\n", shell_info->heredoc_cnt);
+	// 	free_token(token);
+	// 	return (EXIT_FAILURE);
+	// }
 	t_token *current_token;
 	current_token = token;
     while (current_token)
@@ -20,6 +37,11 @@ int	parse_all(t_shell *shell_info, char *str)
         printf("Type: %d, Value: [%s]\n", current_token->type, current_token->value);
         current_token = current_token->next;
     }
+
+	// parse_quote(&token);
+
+	// token test code
+	
 	
 	// 5. token 순회하면서 invalid token 삭제
 	// 6. token 없거나 에러 걸리면 token free하고 fail return -> 나중에
@@ -28,7 +50,7 @@ int	parse_all(t_shell *shell_info, char *str)
 	// init_cmd(&shell_info->cmd, token);
 	// 9. cmd argument 합치기
 
-	 shell_info->cmd = tokens_to_cmds(token);
+	//  shell_info->cmd = tokens_to_cmds(token);
 
 //	if (cmd != NULL)
 //    {
@@ -41,5 +63,6 @@ int	parse_all(t_shell *shell_info, char *str)
 
 //	printf("cmd: %s\n", shell_info->cmd->cmd_args[0]);
 //	printf("cmd: %s\n", shell_info->cmd->cmd_args[1]);
+	
 	return (SUCCESS);
 }
