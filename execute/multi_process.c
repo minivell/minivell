46aa5f8e7bc56544e3f2064cbd198f88f0d4b404
gpffh20 <eunbin584836@gmail.com>
2024-01-12 02:12:24 +0900

    comment: 테스트코드 주석처리

--- execute/multi_process.c
+++ execute/multi_process.c
@@ -29,14 +29,14 @@ void	exec_child_process(t_exec *exec_info, t_cmd *cmd, int order, int last_child
 		return ; // error
 	close(exec_info->pipe[O_STREAM]);
 
-	// for test {
-	t_redir *node;
-
-	node = (t_redir*)malloc(sizeof(t_redir));
-	if(node == NULL){
-		return ;
-	}
-	// }
+//	// for test {
+//	t_redir *node;
+//
+//	node = (t_redir*)malloc(sizeof(t_redir));
+//	if(node == NULL){
+//		return ;
+//	}
+//	// }
 //	set_for_redir(exec_info, cmd->redir);
 	exec_cmd(cmd->cmd_args, exec_info);
 }
