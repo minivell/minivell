c928ababdfac5a118c82ee7d497aff3c560d49f9
gykoh42 <karmakoh42@naver.com>
2024-01-12 20:25:23 +0900

    feat: init_shell 함수 추가

---
+++ parse/init.c
@@ -0,0 +1,12 @@
+#include "../minishell.h"
+
+void init_shell(t_shell *shell_info)
+{
+	if (!shell_info)
+		return;
+	shell_info->pipe_cnt = 0;
+	shell_info->heredoc_cnt = 0;
+	shell_info->redir = NULL;
+	shell_info->env = NULL;
+	shell_info->cmd = NULL;
+}
