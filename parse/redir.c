86c8eb0ee02db23df6b1c64c20d76bf9b766d2f9
gykoh42 <karmakoh42@naver.com>
2024-01-12 20:26:41 +0900

    feat: append_redir_to_shell 함수 추가

--- parse/redir.c
+++ parse/redir.c
@@ -2,47 +2,68 @@
 
 t_redir *new_redir(t_type type, char *filename)
 {
-    t_redir *new;
+	t_redir *new;
 
-    new = (t_redir *)malloc(sizeof(t_redir));
-    if (!new)
-        return (NULL);
-    new->type = type;
-    new->filename = filename;
-    new->next = NULL;
-    return (new);
+	new = (t_redir *)malloc(sizeof(t_redir));
+	if (!new)
+		return (NULL);
+	new->type = type;
+	new->filename = filename;
+	new->next = NULL;
+	return (new);
 }
 
 void    add_back_redir(t_redir **redir, t_redir *new)
 {
-    t_redir *curr;
+	t_redir *curr;
 
-    if (!redir || !new)
-        return ;
-    if (!*redir)
-    {
-        *redir = new;
-        return ;
-    }
-    curr = *redir;
-    while (curr->next)
-        curr = curr->next;
-    curr->next = new;
+	if (!redir || !new)
+		return ;
+	if (!*redir)
+	{
+		*redir = new;
+		return ;
+	}
+	curr = *redir;
+	while (curr->next)
+		curr = curr->next;
+	curr->next = new;
 }
 
 void    free_redir(t_redir *redir)
 {
-    t_redir *curr;
-    t_redir *tmp;
+	t_redir *curr;
+	t_redir *tmp;
 
-    if (!redir)
-        return ;
-    curr = redir;
-    while (curr)
-    {
-        tmp = curr->next;
-        free(curr->filename);
-        free(curr);
-        curr = tmp;
-    }
+	if (!redir)
+		return ;
+	curr = redir;
+	while (curr)
+	{
+		tmp = curr->next;
+		free(curr->filename);
+		free(curr);
+		curr = tmp;
+	}
+}
+
+void append_redir_to_shell(t_shell *shell_info, t_token *tokens)
+{
+	t_token *current_token = tokens;
+	t_redir *new;
+
+	while (current_token)
+	{
+		if (current_token->type >= OUT_REDIR && current_token->type <= HEREDOC)
+		{
+			if (current_token->next && current_token->next->type == FILENAME)
+			{
+				char *filename = ft_strdup(current_token->next->value);
+				new = new_redir(current_token->type, filename);
+				add_back_redir(&(shell_info->redir), new);
+				current_token = current_token->next;
+			}
+		}
+		current_token = current_token->next;
+	}
 }
