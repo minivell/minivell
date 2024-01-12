29f322079bc3effef00b0975e79a6b2a3aaa3e5c
gpffh20 <eunbin584836@gmail.com>
2024-01-12 09:09:56 +0900

    fix: cmd가 없을 때 처리

--- execute/exec_cmd.c
+++ execute/exec_cmd.c
@@ -4,6 +4,8 @@ int	exec_cmd(char **cmd_args, t_exec *exec_info)
 {
 	char	*cmd_path;
 
+	if (cmd_args[0] == NULL)
+		return (SUCCESS);
 	cmd_path = get_cmd_path(cmd_args[0], exec_info->path);
 	if (execve(cmd_path, cmd_args, make_new_env(exec_info)) == FAILURE)
 		return (FAILURE);
