260d54c2d6e9adcee34dcc86ff09300805f9b5e1
Gyeonga Koh <gykoh@cx2r8s2.42seoul.kr>
2024-01-13 20:41:25 +0900

    feat: remove_outer_quotes 함수 추가

--- parse/quote.c
+++ parse/quote.c
@@ -14,7 +14,38 @@ int check_quote(t_quote *quote, char c)
     return (quote->quote_flag);
 }
 
-// void 	parse_quote(t_token **token)
-// {
-//     return ;
-// }
+void remove_outer_quotes(t_token **token_tmp) 
+{
+    t_token *token = *token_tmp;
+    while (token)
+    {
+        char *str = token->value;
+        int len = ft_strlen(str);
+        char *new_str = malloc(len + 1);
+        int new_index = 0;
+        for (int i = 0; i < len; i++) 
+        {
+            if (str[i] == '\"' || str[i] == '\'') 
+            {
+                char quote_type = str[i];
+                int j = i + 1;
+                while (j < len && str[j] != quote_type) 
+                    j++;
+                if (j < len) 
+                {
+                    i++;
+                    while (i < j) 
+                        new_str[new_index++] = str[i++];
+                } 
+                else 
+                    new_str[new_index++] = str[i];
+            } 
+            else 
+                new_str[new_index++] = str[i];
+        }
+        new_str[new_index] = '\0';
+        strlcpy(str, new_str, len + 1);
+        free(new_str);
+        token = token->next;
+    }
+}
