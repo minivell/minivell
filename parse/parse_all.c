2093ffd1d3676918615bc359a369b8bc56c671f7
Gyeonga Koh <gykoh@cx2r8s2.42seoul.kr>
2024-01-13 22:38:46 +0900

    feat: 파싱 전, 토큰 생성 후 에러 확인 로직 추가

--- parse/parse_all.c
+++ parse/parse_all.c
@@ -5,30 +5,30 @@ int	parse_all(t_shell *shell_info, char *str)
 	t_token *token;
 	token = NULL;
 	init_shell(shell_info);
-	if (error_before_parse(token, str) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
+	if (quote_error(str) != EXIT_SUCCESS)
+        return (EXIT_FAILURE);
 	parse_pipe(&token, str);
 	parse_redir(&token);
 	parse_space(&token);
 	parse_filename(&token);
 	remove_outer_quotes(&token);
-	// 앞으로 구현해야 할 것
-	// 5. token 순회하면서 invalid token 삭제 (token 없거나 에러 걸리면 token free하고 fail return)
-	// 6. envp update -> 은영 실행부에서 처리
-	// 7. cmd init 및 argument 저장
+	if (validate_token(&token) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+	// 달러 처리해서 value값 치환한 상태로 넘겨주기
 	count_token_type(shell_info, token);
 	shell_info->cmd = tokens_to_cmds(token);
-	
+	//main 에서 set signal로 설정해주고 heredoc -> signal 처리 유의하기
+
 	///////////////////////////출력 확인 코드 모음///////////////////////////////
 
 	///////////////token 출력 확인 코드////////////////////
-	// t_token *current_token;
-	// current_token = token;
-    // while (current_token)
-    // {
-    //     printf("Type: %d, Value: [%s]\n", current_token->type, current_token->value);
-    //     current_token = current_token->next;
-    // }	
+	t_token *current_token;
+	current_token = token;
+    while (current_token)
+    {
+        printf("Type: %d, Value: [%s]\n", current_token->type, current_token->value);
+        current_token = current_token->next;
+    }	
 
 	///////////////////redir 출력 확인 코드///////////////////////////
 	// t_redir *current_redir;
