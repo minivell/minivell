3c1903a7133a96beb0b507b5f9bce1eb8bd5b500
gpffh20 <eunbin584836@gmail.com>
2024-01-13 15:57:02 +0900

    fix: exec구조체의 포인터의 포인터변수를 포인터 변수로 변경

--- execute/init_exec.c
+++ execute/init_exec.c
@@ -6,7 +6,7 @@ t_exec 	*init_exec(t_shell *shell_info)
 
 	exec_info = malloc(sizeof(t_exec));
 	ft_memset(exec_info, 0, sizeof(t_exec));
-	exec_info->env = &(shell_info->env);
+	exec_info->env = shell_info->env;
 	exec_info->path = get_path();
 	return (exec_info);
 }
