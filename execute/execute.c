5404ab023c486c1ff4525ec8944251e02e7f622d
gpffh20 <eunbin584836@gmail.com>
2024-01-13 13:12:53 +0900

    fix: multi process 실행시 종료되는 에러 해결

--- execute/execute.c
+++ execute/execute.c
@@ -2,16 +2,18 @@
 
 int	execute(t_shell *shell_info)
 {
-//	int		origin_stdin;
-//	int		origin_stdout;
+	int		origin_stdin;
+	int		origin_stdout;
 	t_exec *exec_info;
 
 	exec_info = init_exec(shell_info);
-//	origin_stdin = dup(STDIN_FILENO);
-//	origin_stdout = dup(STDOUT_FILENO);
+	origin_stdin = dup(STDIN_FILENO);
+	origin_stdout = dup(STDOUT_FILENO);
 	if (shell_info->pipe_cnt == 0)
 		single_process(shell_info, exec_info);
 	else
 		multi_process(shell_info, exec_info);
+	dup2(origin_stdin, STDIN_FILENO);
+	dup2(origin_stdout, STDOUT_FILENO);
 	return (SUCCESS);
 }
