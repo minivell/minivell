53d581c6e21291f243e675818c01ecd32ed3dbb8
gpffh20 <eunbin584836@gmail.com>
2024-01-14 22:41:48 +0900

    fix: strncmp->strcmp로 변경

--- builtin/check_n_exec_builtin.c
+++ builtin/check_n_exec_builtin.c
@@ -5,31 +5,34 @@ int check_n_exec_builtin(t_cmd *cmd_info, t_exec *exec_info)
 	char	*cmd;
 
 	cmd = cmd_info->cmd_args[0];
-	if (ft_strncmp(cmd, "echo", 4) == TRUE)
+	if (!cmd)
+		return (FALSE);
+	if (ft_strcmp(cmd, "echo") == 0)
 	{
+		printf("echo\n");
 		g_exit_code = echo(cmd_info->cmd_args);
 	}
-	else if (ft_strncmp(cmd, "cd", 2) == TRUE)
+	else if (ft_strcmp(cmd, "cd") == 0)
 	{
 		g_exit_code = cd(cmd_info->cmd_args, exec_info);
 	}
-	else if (ft_strncmp(cmd, "pwd", 3) == TRUE)
+	else if (ft_strcmp(cmd, "pwd") == 0)
 	{
 		g_exit_code = pwd();
 	}
-	else if (ft_strncmp(cmd, "export", 6) == TRUE)
+	else if (ft_strcmp(cmd, "export") == 0)
 	{
 		g_exit_code = export(exec_info, cmd_info->cmd_args);
 	}
-	else if (ft_strncmp(cmd, "unset", 5) == TRUE)
+	else if (ft_strcmp(cmd, "unset") == 0)
 	{
 		g_exit_code = unset(exec_info, cmd_info->cmd_args);
 	}
-	else if (ft_strncmp(cmd, "env", 3) == TRUE)
+	else if (ft_strcmp(cmd, "env") == 0)
 	{
 		g_exit_code = env(exec_info);
 	}
-	else if (ft_strncmp(cmd, "exit", 4) == TRUE)
+	else if (ft_strcmp(cmd, "exit") == 0)
 	{
 		g_exit_code = exit_shell();
 	}
