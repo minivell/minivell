c758ffbd044ef5dc5cd7a46104e9afd673f7e55a
gykoh42 <karmakoh42@naver.com>
2024-01-17 14:55:42 +0900

    feat: quote 없이 기본 달러 들어올 경우 치환하는 로직 구현

--- parse/convert_env.c
+++ parse/convert_env.c
@@ -1,16 +1,31 @@
 #include "../minishell.h"
 
-int convert_env_len(char *str, int i)
+char *find_env_value(t_env *env_list, char *key) 
 {
-	int len;
-
-	len = 0;
-	while (str[i] && str[i] != ' ' && str[i] != '$' && str[i] != '\"' && str[i] != '\'' && str[i] != '\\')
+    while (env_list != NULL) 
 	{
-		if (str[i] == '\0')
-			return (len);
-		len++;
-		i++;
-	}
-	return (len);
+        if (ft_strncmp(env_list->key, key, ft_strlen(key) + 1))
+		{
+            return env_list->value;
+        }
+        env_list = env_list->next;
+    }
+    return NULL;
+}
+
+void replace_env_variables_in_token(t_shell *shell_info, t_token *token) 
+{
+    while (token != NULL) {
+        if (token->value[0] == '$') 
+		{
+            char *env_var_name = token->value + 1;
+            char *env_value = find_env_value(shell_info->env, env_var_name);
+
+            if (env_value != NULL) {
+                free(token->value);
+                token->value = ft_strndup(env_value, ft_strlen(env_value));
+            }
+        }
+        token = token->next;
+    }
 }
