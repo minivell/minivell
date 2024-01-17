e3a04cac79ce7993fd885c65cadccd17c6461b18
gykoh42 <karmakoh42@naver.com>
2024-01-17 14:54:51 +0900

    style: cmd.c 주석 제거

--- parse/cmd.c
+++ parse/cmd.c
@@ -10,8 +10,8 @@ t_cmd *tokens_to_cmds(t_token *tokens)
         if (tokens->type == PIPE || current_cmd == NULL)
         {
             t_cmd *new_cmd = (t_cmd *)malloc(sizeof(t_cmd));
-            new_cmd->cmd_args = (char **)malloc(sizeof(char *));  // NULL 포인터를 위한 공간 할당
-            new_cmd->cmd_args[0] = NULL;  // 첫 번째 요소를 NULL로 초기화
+            new_cmd->cmd_args = (char **)malloc(sizeof(char *));
+            new_cmd->cmd_args[0] = NULL;
             new_cmd->cmd_cnt = 0;
             new_cmd->redir = NULL;
             new_cmd->next = NULL;
@@ -29,8 +29,6 @@ t_cmd *tokens_to_cmds(t_token *tokens)
                 continue;
             }
         }
-
-        // 리다이렉션 토큰을 처리합니다
         if (tokens->type >= OUT_REDIR && tokens->type <= HEREDOC)
         {
             if (tokens->next && tokens->next->type == FILENAME)
@@ -38,14 +36,12 @@ t_cmd *tokens_to_cmds(t_token *tokens)
                 char *filename = ft_strdup(tokens->next->value);
                 t_redir *new = new_redir(tokens->type, filename);
                 add_back_redir(&(current_cmd->redir), new);
-                tokens = tokens->next;  // 파일 이름 토큰을 건너뜁니다
+                tokens = tokens->next;
             }
         }
         else
         {
-            char **new_args = (char **)malloc(sizeof(char *) * (current_cmd->cmd_cnt + 2));  // +1 개의 인자와 NULL을 위한 공간
-
-            // 기존 인자 복사
+            char **new_args = (char **)malloc(sizeof(char *) * (current_cmd->cmd_cnt + 2));
             if (current_cmd->cmd_args)
             {
                 ft_memcpy(new_args, current_cmd->cmd_args, sizeof(char *) * current_cmd->cmd_cnt);
@@ -53,14 +49,12 @@ t_cmd *tokens_to_cmds(t_token *tokens)
             }
 
             new_args[current_cmd->cmd_cnt] = ft_strdup(tokens->value);
-            new_args[current_cmd->cmd_cnt + 1] = NULL;  // 배열의 끝에 NULL 추가
-
+            new_args[current_cmd->cmd_cnt + 1] = NULL;
             current_cmd->cmd_args = new_args;
             current_cmd->cmd_cnt++;
         }
 
         tokens = tokens->next;
     }
-
     return head_cmd;
 }
