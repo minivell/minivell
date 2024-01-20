b7e8d0ed89ae120609ad1d0f60059f1ac7dceb29
gpffh20 <eunbin584836@gmail.com>
2024-01-20 15:51:56 +0900

    feat: command not found 에러 처리

--- execute/exec_cmd.c
+++ execute/exec_cmd.c
@@ -1,17 +1,22 @@
 #include "../minishell.h"
 
-int	exec_cmd(char **cmd_args, t_exec *exec_info)
+int	exec_cmd(t_cmd *cmd, t_exec *exec_info)
 {
 	char	*cmd_path;
 
 	set_signal(DEFAULT, DEFAULT);
-	if (cmd_args[0] == NULL)
+	if (cmd->cmd_args[0] == NULL)
 		return (SUCCESS);
-	cmd_path = get_cmd_path(cmd_args[0], exec_info->path);
-	if (execve(cmd_path, cmd_args, make_new_env(exec_info)) == FAILURE)
+	if (check_n_exec_builtin(cmd, exec_info, TRUE) == TRUE)
+		return (SUCCESS);
+	cmd_path = get_cmd_path(cmd->cmd_args[0], exec_info->path);
+	if (execve(cmd_path, cmd->cmd_args, make_new_env(exec_info)) == FAILURE)
 	{
-		ft_putstr_fd("command not found: \n", STDERR_FILENO);
-		return (FAILURE);
+		g_exit_code = 127;
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd(cmd->cmd_args[0], STDERR_FILENO);
+		ft_putstr_fd(": command not found\n", STDERR_FILENO);
+		exit(g_exit_code);
 	}
 	return (SUCCESS);
 }
