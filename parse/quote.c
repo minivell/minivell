9c8a2d983d70edc9adf6719450149cdcf85c7bab
gykoh42 <karmakoh42@naver.com>
2024-01-20 14:14:11 +0900

    style: space를 tab으로 전환

--- parse/quote.c
+++ parse/quote.c
@@ -2,64 +2,64 @@
 
 int check_quote(t_quote *quote, char c)
 {
-    if (quote->quote_flag == FALSE && (c == '\'' || c == '\"'))
-    {
-        quote->quote_flag = TRUE;
-        quote->quote = c;
-    }
-    else if (quote->quote_flag == TRUE && c == quote->quote)
-    {
-        quote->quote_flag = FALSE;
-    }
-    return (quote->quote_flag);
+	if (quote->quote_flag == FALSE && (c == '\'' || c == '\"'))
+	{
+		quote->quote_flag = TRUE;
+		quote->quote = c;
+	}
+	else if (quote->quote_flag == TRUE && c == quote->quote)
+	{
+		quote->quote_flag = FALSE;
+	}
+	return (quote->quote_flag);
 }
 
 int find_matching_quote(const char *str, int start_index, char quote_type)
 {
-    int len = ft_strlen(str);
-    for (int j = start_index; j < len; j++) 
-    {
-        if (str[j] == quote_type)
-            return j;
-    }
-    return len;
+	int len = ft_strlen(str);
+	for (int j = start_index; j < len; j++)
+	{
+		if (str[j] == quote_type)
+			return j;
+	}
+	return len;
 }
 
-void remove_quotes_from_string(char *str, char *new_str) 
+void remove_quotes_from_string(char *str, char *new_str)
 {
-    int len = ft_strlen(str);
-    int new_index = 0;
-    for (int i = 0; i < len; i++) 
-    {
-        if (str[i] == '\"' || str[i] == '\'') 
-        {
-            int j = find_matching_quote(str, i + 1, str[i]);
-            if (j < len)
-            {
-                i++;
-                while (i < j)
-                    new_str[new_index++] = str[i++];
-            } 
-            else
-                new_str[new_index++] = str[i];
-        } 
-        else 
-            new_str[new_index++] = str[i];
-    }
-    new_str[new_index] = '\0';
+	int len = ft_strlen(str);
+	int new_index = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] == '\"' || str[i] == '\'')
+		{
+			int j = find_matching_quote(str, i + 1, str[i]);
+			if (j < len)
+			{
+				i++;
+				while (i < j)
+					new_str[new_index++] = str[i++];
+			}
+			else
+				new_str[new_index++] = str[i];
+		}
+		else
+			new_str[new_index++] = str[i];
+	}
+	new_str[new_index] = '\0';
 }
 
-void remove_outer_quotes(t_token **token) 
+void remove_outer_quotes(t_token **token)
 {
-    t_token *token_tmp = *token;
-    while (token_tmp) 
-    {
-        char *str = token_tmp->value;
-        int len = ft_strlen(str);
-        char *new_str = malloc(len + 1);
-        remove_quotes_from_string(str, new_str);
-        ft_strlcpy(str, new_str, len + 1);
-        free(new_str);
-        token_tmp = token_tmp->next;
-    }
+	t_token *token_tmp = *token;
+	while (token_tmp)
+	{
+		char *str = token_tmp->value;
+		int len = ft_strlen(str);
+		char *new_str = malloc(len + 1);
+		remove_quotes_from_string(str, new_str);
+		ft_strlcpy(str, new_str, len + 1);
+		free(new_str);
+		token_tmp = token_tmp->next;
+	}
 }
