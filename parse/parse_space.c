875bdcc63b3eeb10c072d712e4f2969f433853b4
gykoh42 <karmakoh42@naver.com>
2024-01-20 15:29:29 +0900

    refactor: parse_space 함수 분리

--- parse/parse_space.c
+++ parse/parse_space.c
@@ -5,38 +5,55 @@ int ft_isspace(int c)
 	return (c == ' ' || c == '\t');
 }
 
+int is_space_outside_quotes(char c, t_quote quote)
+{
+	return ft_isspace(c) && !quote.quote_flag;
+}
+
+void update_quote_flag(char c, t_quote *quote)
+{
+	if ((c == '\'' || c == '\"') && (!quote->quote_flag || c == quote->quote))
+	{
+		quote->quote_flag = !quote->quote_flag;
+		quote->quote = c;
+	}
+}
+
+t_token *create_new_token(t_type type, char *start, char *end)
+{
+	return new_token(type, ft_strndup(start, end - start));
+}
+
+void tokenize_string(char *str, t_type type, t_token **new_token_list)
+{
+	t_quote quote = { .quote_flag = FALSE, .quote = '\0' };
+
+	while (*str)
+	{
+		while (is_space_outside_quotes(*str, quote))
+			str++;
+		char *start = str;
+		while (*str && (!is_space_outside_quotes(*str, quote)))
+		{
+			update_quote_flag(*str, &quote);
+			str++;
+		}
+		if (str > start)
+		{
+			t_token *new_node = create_new_token(type, start, str);
+			add_back_token(new_token_list, new_node);
+		}
+	}
+}
+
 void parse_space(t_token **token)
 {
 	t_token *tmp = *token;
 	t_token *new_token_list = NULL;
-	t_token *new_node;
-	t_quote quote;
-	quote.quote_flag = FALSE;
 
 	while (tmp)
 	{
-		char *str = tmp->value;
-		while (*str)
-		{
-			while (!quote.quote_flag && ft_isspace(*str))
-				str++;
-
-			char *start = str;
-			while (*str && (!ft_isspace(*str) || quote.quote_flag))
-			{
-				if ((*str == '\'' || *str == '\"') && (quote.quote_flag == FALSE || *str == quote.quote))
-				{
-					quote.quote_flag = !quote.quote_flag;
-					quote.quote = *str;
-				}
-				str++;
-			}
-			if (str > start)
-			{
-				new_node = new_token(tmp->type, ft_strndup(start, str - start));
-				add_back_token(&new_token_list, new_node);
-			}
-		}
+		tokenize_string(tmp->value, tmp->type, &new_token_list);
 		tmp = tmp->next;
 	}
 	free_token(*token);
