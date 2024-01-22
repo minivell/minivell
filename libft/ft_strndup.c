a98e181a909fb3e9a0670cb293ec206df05c99dc
eushin <eunbin584836@gmail.com>
2024-01-22 18:31:36 +0900

    fix: norm error catch

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -1,22 +1,23 @@
 #include "libft.h"
 
-char *ft_strndup(const char *s, size_t n)
+char	*ft_strndup(const char *s, size_t n)
 {
-    char *dup;
-    size_t len;
+	char	*dup;
+	size_t	len;
+	size_t	i;
 
-    len = 0;
-    while (s[len] && len < n)
-        len++;
-
-    dup = (char *)malloc(sizeof(char) * (len + 1));
-    if (!dup)
-        return NULL;
-
-    for (size_t i = 0; i < len; i++)
-        dup[i] = s[i];
-
-    dup[len] = '\0';
-
-    return dup;
+	len = 0;
+	while (s[len] && len < n)
+		len++;
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
 }
