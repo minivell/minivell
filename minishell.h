f869f7155c79a4230c2b4571ea1fb122f33e255d
eushin <eunbin584836@gmail.com>
2024-01-22 17:44:16 +0900

    fix: norm error catch

--- minishell.h
+++ minishell.h
@@ -1,92 +1,92 @@
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#define SUCCESS 0
-#define FAILURE -1
+# define SUCCESS 0
+# define FAILURE -1
 
-#define DEFAULT   0
-#define IGNORE    1
-#define MINIVELL  2
-#define HRD_CHILD 3
+# define DEFAULT   0
+# define IGNORE    1
+# define MINIVELL  2
+# define HRD_CHILD 3
 
-#define TRUE 1
-#define FALSE 0
+# define TRUE 1
+# define FALSE 0
 
-#define O_STREAM 1
-#define I_STREAM 0
+# define O_STREAM 1
+# define I_STREAM 0
 
-#include "struct.h"
-#include "./libft/libft.h"
+# include "struct.h"
+# include "./libft/libft.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/termios.h>
-#include <sys/wait.h>
-#include <sys/ioctl.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/termios.h>
+# include <sys/wait.h>
+# include <sys/ioctl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-extern int g_exit_code;
-
-void free_str_arr(char **arr);
+extern int	g_exit_code;
 
 /*parse*/
 
 /*parse_all.c*/
-int parse_all(t_shell *shell_info, char *str);
+int		parse_all(t_shell *shell_info, char *str);
 
 /*init.c*/
-void init_shell(t_shell *shell_info);
+void	init_shell(t_shell *shell_info);
 
 /*env.c*/
-char *find_key(char *value, int *i, int *start_idx);
-char *find_value(t_env *env_list, char *key, int *flag);
+char	*find_key(char *value, int *i, int *start_idx);
+char	*find_value(t_env *env_list, char *key, int *flag);
 t_env	*new_env(char *key, char *value);
 void	env_add_back(t_env **env, t_env *new);
 void	init_env(t_env **env, char *envp[]);
 
 /*convert_env.c*/
-char *ft_strjoin_free(char *s1, const char *s2);
+char	*ft_strjoin_free(char *s1, const char *s2);
 char	*join_and_free(char *s1, char *s2, int free_s1);
 char	*process_segment(char *str, int start, int end, char **res);
-void process_env_var(t_env *env_list, t_env_process *env_proc);
-void	process_token(t_env *env_list, t_token *token, char quote_flag, char *res);
-void replace_env_in_token(t_env *env_list, t_token *token);
+void	process_env_var(t_env *env_list, t_env_process *env_proc);
+void	process_token(t_env *env_list, \
+	t_token *token, char quote_flag, char *res);
+void	replace_env_in_token(t_env *env_list, t_token *token);
 
 /*parse_pipe.c*/
-void parse_pipe(t_token **token, char *str);
+void	parse_pipe(t_token **token, char *str);
 
 /*parse_redir.c*/
-void parse_redir(t_token **token);
-void parse_filename(t_token **token);
-void replace_token(t_token **token_list, t_token *old_token, t_token *new_tokens);
-void parse_and_extract_redir(t_token **token, char *str);
-void create_redir_token(t_token **token, char **str);
+void	parse_redir(t_token **token);
+void	parse_filename(t_token **token);
+void	replace_token(t_token **token_list, \
+	t_token *old_token, t_token *new_tokens);
+void	parse_and_extract_redir(t_token **token, char *str);
+void	create_redir_token(t_token **token, char **str);
 
 /*parse_space.c*/
-int is_space_outside_quotes(char c, t_quote quote);
-void update_quote_flag(char c, t_quote *quote);
-t_token *create_new_token(t_type type, char *start, char *end);
-void tokenize_string(char *str, t_type type, t_token **new_token_list);
-void parse_space(t_token **token);
+int		is_space_outside_quotes(char c, t_quote quote);
+void	update_quote_flag(char c, t_quote *quote);
+t_token	*create_new_token(t_type type, char *start, char *end);
+void	tokenize_string(char *str, t_type type, t_token **new_token_list);
+void	parse_space(t_token **token);
 
 /*parse_error.c*/
-int print_error_msg(void);
-int quote_error(char *str);
+int		print_error_msg(void);
+int		quote_error(char *str);
 
 /*token_check.c*/
-int check_pipe_error(t_token *token);
-int check_redir_sequence_error(t_token *token);
-int check_heredoc_limit(t_token *token);
-int check_redir_filename_error(t_token *token);
-int validate_token(t_token **token);
+int		check_pipe_error(t_token *token);
+int		check_redir_sequence_error(t_token *token);
+int		check_heredoc_limit(t_token *token);
+int		check_redir_filename_error(t_token *token);
+int		validate_token(t_token **token);
 
 /*cmd.c*/
-t_cmd *tokens_to_cmds(t_token *tokens);
+t_cmd	*tokens_to_cmds(t_token *tokens);
 
 /*signal.c*/
 void	hrd_handler(int sig_no);
@@ -94,36 +94,37 @@ void	hrd_ign_handler(int sig_no);
 void	set_signal(int sig_int, int sig_quit);
 
 /*redir.c*/
-t_redir *new_redir(t_type type, char *filename);
-void add_back_redir(t_redir **redir, t_redir *new);
-void free_redir(t_redir *redir);
+t_redir	*new_redir(t_type type, char *filename);
+void	add_back_redir(t_redir **redir, t_redir *new);
+void	free_redir(t_redir *redir);
 
 /*quote.c*/
-int check_quote(t_quote *quote, char c);
-int find_matching_quote(const char *str, int start_index, char quote_type);
-void remove_quotes_from_string(char *str, char *new_str);
+int		check_quote(t_quote *quote, char c);
+int		find_matching_quote(const char *str, int start_index, char quote_type);
+void	remove_quotes_from_string(char *str, char *new_str);
 char	handle_quotes(char quote_flag, char current_char);
-void remove_outer_quotes(t_token **token);
+void	remove_outer_quotes(t_token **token);
 
 /*token.c*/
-void add_token_if_not_empty(char **start, char **current, t_token **token, t_type type);
-t_token *new_token(t_type type, char *value);
-void add_back_token(t_token **node, t_token *new);
+void	add_token_if_not_empty(char **start, \
+	char **current, t_token **token, t_type type);
+t_token	*new_token(t_type type, char *value);
+void	add_back_token(t_token **node, t_token *new);
 void	free_token(t_token *token);
-void count_token_type(t_shell *shell_info, t_token *token);
+void	count_token_type(t_shell *shell_info, t_token *token);
 
 /*free.c*/
-void free_redir_list(t_redir *redir);
-void free_str_arr(char **arr);
-void free_str_arr_line(char **arr, int len);
+void	free_redir_list(t_redir *redir);
+void	free_str_arr(char **arr);
+void	free_str_arr_line(char **arr, int len);
 
 /*free2.c*/
-void free_cmd_list(t_cmd *cmd);
-void free_token_list(t_token *token);
-void free_env_list(t_env *env);
+void	free_cmd_list(t_cmd *cmd);
+void	free_token_list(t_token *token);
+void	free_env_list(t_env *env);
 
-char *ft_strjoin_free(char *s1, const char *s2);
-char *find_key(char *value, int *i, int *start_idx);
+char	*ft_strjoin_free(char *s1, const char *s2);
+char	*find_key(char *value, int *i, int *start_idx);
 
 /* execute */
 
@@ -138,7 +139,8 @@ void	exec_cmd(t_cmd *cmd, t_exec *exec_info, int child);
 
 // [execute/exec_process.c]
 void	exec_parents_process(t_exec *exec_info);
-void	exec_child_process(t_exec *exec_info, t_cmd *cmd, int order, int last_child);
+void	exec_child_process(t_exec *exec_info, \
+	t_cmd *cmd, int order, int last_child);
 
 // [execute/execute.c]
 void	execute(t_shell *shell_info);
@@ -150,24 +152,24 @@ char	*get_cmd_path(char *cmd, char **path);
 char	**get_path(void);
 
 // [execute/init_exec.c]
-t_exec 	*init_exec(t_shell *shell_info);
+t_exec	*init_exec(t_shell *shell_info);
 
 // [execute/make_new_env.c]
 char	**make_new_env(t_exec *exec_info);
 
 // [execute/multi_process.c]
-void multi_process(t_shell *shell_info, t_exec *exec_info);
-void exec_child_process(t_exec *exec_info, t_cmd *cmd, int order, int last);
-void exec_parents_process(t_exec *exec_info);
+void	multi_process(t_shell *shell_info, t_exec *exec_info);
+void	exec_child_process(t_exec *exec_info, t_cmd *cmd, int order, int last);
+void	exec_parents_process(t_exec *exec_info);
 
 // [execute/set_for_heredoc.c]
-int	set_for_heredoc(t_shell *shell_info);
+int		set_for_heredoc(t_shell *shell_info);
 
 // [execute/single_process.c]
 void	single_process(t_shell *shell_info, t_exec *exec_info);
 
 // [execute/set_for_redir.c]
-int	set_for_redir(t_exec *exec_info, t_redir *redir);
+int		set_for_redir(t_exec *exec_info, t_redir *redir);
 
 // [execute/unlink_heredoc.c]
 void	unlink_heredoc(void);
@@ -175,31 +177,30 @@ void	unlink_heredoc(void);
 // [execute/wait_child.c]
 void	wait_child(int child_cnt, pid_t last_child);
 
-
 /* builtin */
 
 // [builtin/cd.c]
-int	cd(char **args, t_exec *exec_info);
+int		cd(char **args, t_exec *exec_info);
 
 // [builtin/check_n_exec_builtin.c]
-int	check_n_exec_builtin(t_cmd *cmd_info, t_exec *exec_info, int exit_flag);
+int		check_n_exec_builtin(t_cmd *cmd_info, t_exec *exec_info, int exit_flag);
 
 // [builtin/echo.c]
-int	echo(char **args);
+int		echo(char **args);
 
 // [builtin/env.c]
-int	env(t_exec *exec_info, char **cmd_args);
+int		env(t_exec *exec_info, char **cmd_args);
 
 // [builtin/exit.c]
-int exit_shell(char **cmd_args, int exit_flag);
+int		exit_shell(char **cmd_args, int exit_flag);
 
 // [builtin/export.c]
-int	export(t_exec *exec_info, char **args);
+int		export(t_exec *exec_info, char **args);
 
 // [builtin/pwd.c]
-int	pwd(void);
+int		pwd(void);
 
 // [builtin/unset.c]
-int	unset(t_exec *exec_info, char **args);
+int		unset(t_exec *exec_info, char **args);
 
 #endif
